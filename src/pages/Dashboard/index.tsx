841154abac3f244b5cdddf6a264fb71ca0cf50a4
bmpschneider <brunopschneider@gmail.com>
2021-11-15 08:18:35 -0300

    feat: implement dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles';
+import { Title, Form, Repos } from './styles';
+import logo from '../../assets/logo.svg'
 
 export const Dashboard: React.FC = () => {
   return (
-      <Title>Dashboard</Title>
+      <>
+      <img src={logo} alt="GitCollection" />
+      <Title>Gibhub Repository Catalog</Title>
+
+      <Form>
+          <input placeholder="username/repository_name" />
+          <button type="submit">Search</button>
+      </Form>
+
+      <Repos>
+          <a href="/repositories">
+              <img src="https://avatars.githubusercontent.com/u/17718751?v=4" alt="repository" />
+              <div>
+                  <strong>brunodeveloper/gitcollection</strong>
+                  <p>Repositório do GitHub GitCollection</p>
+              </div>
+              <FiChevronRight size={20}></FiChevronRight>
+          </a>
+      </Repos>
+      </>
   );
 };
